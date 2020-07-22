6ec71fbc0fcce58f46dcc3d27812a94c9dab8a54
Walmyr <wlsf82@gmail.com>
2020-07-22 13:46:06 +0200

    Update description with linkedin and medium links

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hi, I'm <strong>Walmyr Filho</strong>. I'm a software engineer that loves testing. I'm also a clean coder, blogger, YouTuber, writer, online teacher, mentor, speaker, an active member of the testing automation and JavaScript communities, and a fan of good music, tattoos, and skateboarding. Follow me on <a href="https://twitter.com/@walmyrlimaesilv">Twitter.</a></p>
+        <p>Hi, I'm <strong>Walmyr Filho</strong>. I'm a software engineer that loves testing. I'm also a clean coder, blogger, YouTuber, writer, online teacher, mentor, speaker, an active member of the testing automation and JavaScript communities, and a fan of good music, tattoos, and skateboarding. Follow me on <a href="https://twitter.com/@walmyrlimaesilv">Twitter</a>, <a href="https://www.linkedin.com/in/walmyr-lima-e-silva-filho-147a9110a/">LinkedIn</a>, and on <a href="https://medium.com/@walmyrlimaesilv">Medium</a>.</p>
       </section>
     </Layout>
   )
