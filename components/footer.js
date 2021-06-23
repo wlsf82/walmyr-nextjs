5f3ae2ab99c2218c5910193602377fa523200480
Walmyr <wlsf82@gmail.com>
2021-06-23 20:37:56 +0200

    Minor footer adjustment

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,7 @@ import utilStyles from '../styles/utils.module.css'
 export default function Footer() {
   return (
     <footer className={utilStyles.footer}>
-      <p>Copyleft 😜 2020 - 2021. Walmyr Filho.</p>
+      <p>Copyleft 😜 2021. Walmyr Filho.</p>
       <p>See this project <a href="https://github.com/wlsf82/walmyr-nextjs">on GitHub</a>.</p>
     </footer>
   )
