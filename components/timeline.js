c424bab7d054dc5d46240dffb4aa1b0296954ee1
Walmyr <wlsf82@gmail.com>
2023-02-23 04:19:20 +0100

    Update timeline event

--- components/timeline.js
+++ components/timeline.js
@@ -107,7 +107,7 @@ const achievements = [
     year: '2023',
     events: [
       'Reached 12.5K students on Udemy ⬆️',
-      'Got to 19K enrollments on my Udemy courses ✨',
+      'Got to 20K enrollments on my Udemy courses ✨',
       'Reached 4K subscribers on YouTube 🥳',
       'Reached the mark of 100 Live Streams on YouTube 💯',
     ]
