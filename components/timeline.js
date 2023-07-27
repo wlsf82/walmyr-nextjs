aaedbbfbae9c930ce060e8885a3f495f0e8b8b7c
Walmyr <wlsf82@gmail.com>
2023-07-27 18:32:36 +0200

    Update number of enrollments on Udemy

--- components/timeline.js
+++ components/timeline.js
@@ -107,7 +107,7 @@ const achievements = [
     year: '2023',
     events: [
       'Reached 14K students on Udemy ⬆️',
-      'Got to 23K enrollments on my Udemy courses ✨',
+      'Got to 24K enrollments on my Udemy courses ✨',
       'Reached 5K subscribers on YouTube 🥳',
       'Reached the mark of 100 Live Streams on YouTube 💯',
     ]
