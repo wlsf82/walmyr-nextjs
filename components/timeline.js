c8a5c44e2111bbe7d0f0569eea3ac26f82ad4540
Walmyr <wlsf82@gmail.com>
2023-09-06 04:45:54 +0200

    Update timeline
    Number of enrollments.

--- components/timeline.js
+++ components/timeline.js
@@ -107,7 +107,7 @@ const achievements = [
     year: '2023',
     events: [
       'Reached 15K students on Udemy ⬆️',
-      'Got to 24K enrollments on my Udemy courses ✨',
+      'Got to 25K enrollments on my Udemy courses ✨',
       'Reached 5K subscribers on YouTube 🥳',
       'Reached the mark of 100 Live Streams on YouTube 💯',
       'The Talking About Testing channel is sponsored by Zero Defect',
