860e166b731c8fefa023ef7af0c879ae04e176ee
Walmyr <wlsf82@gmail.com>
2023-09-11 21:01:51 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hi, I'm <strong>Walmyr</strong>. I'm a software engineer who loves testing. I'm also a clean coder, blogger, YouTuber, podcaster, Cypress.io ambassador, online instructor, mentor, speaker, an active member of the testing automation and JavaScript communities, and a fan of good music, tattoos, gardening, vegan 🌱 food, and skateboarding. Follow me on <a href="https://www.linkedin.com/in/walmyr-lima-e-silva-filho">LinkedIn</a>, <a href="https://medium.com/@walmyrlimaesilv">Medium</a>, and on <a href="https://dev.to/walmyrlimaesilv">DEV Community</a>.</p>
+        <p>Hi, I'm <strong>Walmyr</strong>. I'm a software engineer who loves testing. I'm also a clean coder, blogger, YouTuber, podcaster, Cypress.io Ambassador, online instructor, mentor, speaker, an active member of the testing automation and JavaScript communities, and a fan of good music, tattoos, gardening, vegan 🌱 food, and skateboarding. Follow me on <a href="https://www.linkedin.com/in/walmyr-lima-e-silva-filho">LinkedIn</a>, <a href="https://medium.com/@walmyrlimaesilv">Medium</a>, and on <a href="https://dev.to/walmyrlimaesilv">DEV Community</a>.</p>
       </section>
     </Layout>
   )
