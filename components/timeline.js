1b450973c0672ef899c4191af3d33504599896c6
Walmyr <wlsf82@gmail.com>
2023-10-18 02:04:00 +0200

    Update number of students on udemy

--- components/timeline.js
+++ components/timeline.js
@@ -106,7 +106,7 @@ const achievements = [
   {
     year: '2023',
     events: [
-      'Reached 15K students on Udemy ⬆️',
+      'Reached 16K students on Udemy ⬆️',
       'Got to 25K enrollments on my Udemy courses ✨',
       'Reached 5K subscribers on YouTube 🥳',
       'Reached the mark of 100 Live Streams on YouTube 💯',
