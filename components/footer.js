c29d17f15481220438fed8601d5f324900ecd5e5
Walmyr <wlsf82@gmail.com>
2024-01-01 19:52:41 +0100

    Update year on footer component

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,7 @@ import { footer } from '../styles/utils.module.css'
 export default function Footer() {
   return (
     <footer className={ footer }>
-      <p>Copyleft 😜 2023. Walmyr.</p>
+      <p>Copyleft 😜 2024. Walmyr.</p>
       <p>See this project <a href="https://github.com/wlsf82/walmyr-nextjs">on GitHub</a>.</p>
     </footer>
   )
