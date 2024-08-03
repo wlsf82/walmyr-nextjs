b8fca11d3d381ddc4319bedc3bc9f0e023a8ab56
Walmyr <wlsf82@gmail.com>
2024-08-03 20:56:43 +0200

    Update achievements order

--- components/achievements.js
+++ components/achievements.js
@@ -125,9 +125,9 @@ const achievements = [
       'Reached 6K subscribers on YouTube',
       'Reached 19K students on Udemy ⬆️',
       'Got to 30K enrollments on my Udemy courses ✨',
-      'Finished the first and second Test Design Masterclass 👨‍🏫',
       'Reached 1K followers on GitHub 🦑🐈‍⬛🐙',
       'Launch of my first course at the Hotmart platform',
+      'Finished the first and second Test Design Masterclass 👨‍🏫',
     ]
   }
 ]
