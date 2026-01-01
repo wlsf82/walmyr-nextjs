347db7b5e9d66cc71ae27e0c970f6e5a27ef5333
Walmyr <wlsf82@gmail.com>
2026-01-01 17:32:12 +0100

    Update footer copy

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,7 @@ import { footer } from '../styles/utils.module.css'
 export default function Footer() {
   return (
     <footer className={ footer }>
-      <p>Copyleft 😜 2025. Walmyr.</p>
+      <p>Copyleft 😜 2026. Walmyr.</p>
       <p>See this project <a href="https://github.com/wlsf82/walmyr-nextjs">on GitHub</a>.</p>
     </footer>
   )
